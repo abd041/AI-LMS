f92710ccddad4e1ea89b973d187c03e4e7f66c25
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2025-10-14 20:37:49 +0500

    Installed shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <div className='text-2xl underline'>Welcome to My SAAS </div>
+      <Button>Let's get started</Button>
+    </div>
   )
 }
 
